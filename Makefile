f312e05682fd1c64f00d1b51c350489a275437b1
Matt Sampson <sampson7185@gmail.com>
2016-02-01 22:44:04 -0500

    Section one done (?) not sure if bg ps work properly yet

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -Wall -pedantic -std=c99 -g -c
 all: myShell
 
 myShell: myShell.o
-	$(CC) myShell.o -o myShell
+	$(CC) myShell.o -o MaSh
 
 myShell.o: myShell.c
 	$(CC) $(CFLAGS) myShell.c
--- myShell.c
+++ myShell.c
@@ -7,34 +7,59 @@
 #include <string.h>     /* Strings */
 
 int parseInput(char *input, char **args) {
-	char *temp = malloc(sizeof(char)*10);
-	strcpy(temp,"dicks");
-	args[0] = temp;
-	return 0;
+	char *token;
+	int argCount = 0;
+
+	token = strtok(input," \n");
+	while (token != NULL){
+		char *temp = malloc(sizeof(char)*10);
+		strcpy(temp,token);
+		token = strtok(NULL, " \n");
+		args[argCount] = temp;
+		argCount++;
+	}
+	return argCount;
 }
 
 int main(int argc, char const *argv[])
 {
 	char *input = malloc(sizeof(char)*50);
-	char *args[64];
 	pid_t childpid;
-	int status;
+	int status, execStatus, runInBG;
 
-	printf("Welcome to SampShell, enter a command or type h for help.\n");
+	printf("Welcome to MaSh, enter a command or type h for help.\n");
 	printf(">");
-	scanf("%s", input);
-	while(strcmp(input,"exit") != 0) {
+	fgets(input,50,stdin);
+	while(strcmp(input,"exit\n") != 0) {
+		char **args = malloc(sizeof(char*)*10);
 		childpid = fork();
 		if (childpid >= 0) { //fork success
 			status = parseInput(input,args);
-			//printf("%s\n", args[0]);
+			if ((*args[status - 1]) == '&') {
+				runInBG = 1;
+				args[status - 1] = NULL;
+			}
 			if (childpid == 0) {
+				execStatus = execvp(*args,args);
+				if (execStatus < 0) {
+					printf("Command \"%s\" not found.\n", input);
+				}
 				exit(0);
+			}
+			else {
+				if (runInBG == 1) {
+					waitpid(-1,&status,WNOHANG | WUNTRACED);
+				}
+				else
+					waitpid(childpid,&status,0);			
 			}
 		}
 		printf(">");
-		scanf("%s", input);
+		fgets(input,50,stdin);
+		free(args);
 	}
 	printf("logout\n");
+	printf("\n");
+	printf("[Process completed]\n");
 	exit(0);
 }
