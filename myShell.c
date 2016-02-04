d41eb9a3aa19d417310399b50869fb741d355295
Matt Sampson <sampson7185@gmail.com>
2016-02-04 15:03:12 -0500

    still working on piping

--- myShell.c
+++ myShell.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>       /* Input/Output */
 #include <stdlib.h>      /* General Utilities */
 #include <unistd.h>      /* Symbolic Constants */
@@ -29,25 +30,20 @@ int runInBackground(char **args, int status) {
 	return 0;
 }
 
-int redirectToFromFile(int runInBG, char **args, int status) {
-	if (runInBG == 1) {
-		if ((*args[status - 3]) == '>') {
-			args[status - 3] = NULL;
+int redirectToFromFile(int runInBG, char **args, int status, int *commandTwo) {
+	for (int i = 0; i < status - runInBG; i++) {
+		if ((*args[i]) == '>') {
+			args[i] = NULL;
 			return 1;
 		}
-		else if ((*args[status - 3]) == '<') {
-			args[status - 3] = NULL;
+		else if ((*args[i]) == '<') {
+			args[i] = NULL;
 			return 2;
 		}
-	}
-	else {
-		if ((*args[status - 2]) == '>') {
-			args[status - 2] = NULL;
-			return 1;
-		}
-		else if ((*args[status - 2]) == '<') {
-			args[status - 2] = NULL;
-			return 2;
+		else if ((*args[i]) == '|') {
+			args[i] = NULL;
+			(*commandTwo) = i + 1;
+			return 3;
 		}
 	}
 	return 0;
@@ -69,9 +65,26 @@ int readIptFromFile(int runInBG, char **args, int status) {
 	FILE *fp;
 	int execStatus;
 	if (runInBG == 1)
-		fp = freopen(args[status - 2], "r", stdout);
+		fp = freopen(args[status - 2], "r", stdin);
 	else 
-		fp = freopen(args[status - 1], "r", stdout);
+		fp = freopen(args[status - 1], "r", stdin);
+	execStatus = execvp(*args,args);
+	fclose(fp);
+	return execStatus;
+}
+
+/*int readIptFromPipe(int pipe, char **args, int commandTwo) {
+	FILE *fp;
+	int execStatus;
+	fp = fdopen(pipe, "r");
+	execStatus = execvp(*args[commandTwo],args[commandTwo]);
+}*/
+
+int writeOptToPipe(int pipe, char **args) {
+	FILE *fp;
+	int execStatus;
+
+	fp = fdopen(pipe, "w");
 	execStatus = execvp(*args,args);
 	fclose(fp);
 	return execStatus;
@@ -80,9 +93,9 @@ int readIptFromFile(int runInBG, char **args, int status) {
 int main(int argc, char const *argv[])
 {
 	char *input = malloc(sizeof(char)*50);
-	pid_t childpid;
-	int status, execStatus;
-	int runInBG = 0, redirToFile = 0;
+	pid_t childpid, pipeChild;
+	int status, execStatus, commandTwo;
+	int runInBG = 0, redirToFile = 0, pipefd[2];
 
 	printf("Welcome to MaSh, enter a command or type h for help.\n");
 	printf(">");
@@ -97,13 +110,34 @@ int main(int argc, char const *argv[])
 			if (status > 1)
 				runInBG = runInBackground(args,status);
 			if (status > 2)
-				redirToFile = redirectToFromFile(runInBG,args,status);
+				redirToFile = redirectToFromFile(runInBG,args,status, &commandTwo);
 			if (childpid == 0) {
 				if (redirToFile == 1) {
 					execStatus = writeOptToFile(runInBG,args,status);
 				}
 				else if (redirToFile == 2) {
 					execStatus = readIptFromFile(runInBG,args,status);
+				}
+				//write code to pipe here
+				else if (redirToFile == 3) {
+					//create pipe
+					if (pipe(pipefd) == -1) {
+						printf("Pipe failed.\n");
+					}
+					pipeChild = fork();
+					if (pipeChild >= 0) {
+						if (pipeChild == 0) {
+							close(pipefd[1]);
+							exit(0);
+						}
+						else {
+							close(pipefd[0]);
+							execStatus = writeOptToPipe(pipefd[1],args);
+						}
+					}
+					else {
+						printf("Fork failed.\n");
+					}
 				}
 				else
 					execStatus = execvp(*args,args);
@@ -119,6 +153,9 @@ int main(int argc, char const *argv[])
 				else
 					waitpid(childpid,&status,0);			
 			}
+		}
+		else {
+			printf("Fork failed.\n");
 		}
 		printf(">");
 		fgets(input,50,stdin);
