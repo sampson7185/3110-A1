d500dba14c68d8ebd9d4e291ff341dd4ee72eb34
Matt Sampson <sampson7185@gmail.com>
2016-02-15 16:41:21 -0500

    final version of the shell program

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -Wall -pedantic -std=c99 -g -c
 all: myShell
 
 myShell: myShell.o
-	$(CC) myShell.o -o MaSh
+	$(CC) myShell.o -o CIS3110sh
 
 myShell.o: myShell.c
 	$(CC) $(CFLAGS) myShell.c
--- myShell.c
+++ myShell.c
@@ -1,3 +1,6 @@
+/*Matt Sampson, February 15th, 2016
+This program is a shell for executing programs. Processes can be started, output and
+input can be controlled with the >, <, and | operators.*/
 #define _GNU_SOURCE
 #include <stdio.h>       /* Input/Output */
 #include <stdlib.h>      /* General Utilities */
@@ -7,7 +10,13 @@
 #include <errno.h>       /* Errors */
 #include <string.h>     /* Strings */
 #include <signal.h>
+#include <ctype.h>
+#include <stdbool.h>
 
+/*This function takes the user input and parses it
+Preconditions: input contains users input, args has been allocated
+Postconditions: args will contain an array of input seperated and without end line character
+Returns: the number of arguments found.*/
 int parseInput(char *input, char **args) {
 	char *token;
 	int argCount = 0;
@@ -23,6 +32,10 @@ int parseInput(char *input, char **args) {
 	return argCount;
 }
 
+/*This function checks if the input line has an &
+Preconditions: user input has already been parsed into args
+Postconditions: The & will be removed from the user input
+Returns: a 1 if the & is found, otherwise a 0*/
 int runInBackground(char **args, int status) {
 	if ((*args[status - 1]) == '&') {
 		args[status - 1] = NULL;
@@ -31,6 +44,11 @@ int runInBackground(char **args, int status) {
 	return 0;
 }
 
+/*This function determines if any file redirection is required
+Preconditions: user input has already been parsed into args
+Postconditions: The redirection symbol will beremoved from the args
+Returns: a 1 for >, a 2 for <, and 3 for | and an int that says where the second
+command starts. returns a 0 if no redirection is desired.*/
 int redirectToFromFile(int runInBG, char **args, int status, int *commandTwo) {
 	for (int i = 0; i < status - runInBG; i++) {
 		if ((*args[i]) == '>') {
@@ -50,6 +68,10 @@ int redirectToFromFile(int runInBG, char **args, int status, int *commandTwo) {
 	return 0;
 }
 
+/*This function opens the file to write the output of the command to
+Preconditions: args has already been parsed
+Postconditions: The command has run and written the output to a file
+Returns: The status after execution to ensure it executed properly*/
 int writeOptToFile(int runInBG, char **args, int status) {
 	FILE *fp;
 	int execStatus;
@@ -62,6 +84,10 @@ int writeOptToFile(int runInBG, char **args, int status) {
 	return execStatus;
 }
 
+/*This function opens the file and reads the file as input to the command
+Preconditions: args has already been parsed
+Postconditions: The command has run with input from the file
+Returns: The status after execution to ensure it executed properly*/
 int readIptFromFile(int runInBG, char **args, int status) {
 	FILE *fp;
 	int execStatus;
@@ -74,28 +100,43 @@ int readIptFromFile(int runInBG, char **args, int status) {
 	return execStatus;
 }
 
+//this is the signal catcher for when the child process finishes executing
+void endSignal(int signal, siginfo_t *signalInfo, void *hold) {
+
+	if (signal == SIGCHLD) {
+		printf("[Process completed]\n");
+	}
+}
+
+//This is the main loop that runs the shell and takes in the commands
 int main(int argc, char const *argv[])
 {
-	char *input = malloc(sizeof(char)*50);
+	char *input;
 	pid_t childpid, pipeChild;
 	int status, execStatus, commandTwo;
 	int runInBG = 0, redirToFile = 0, pipefd[2];
+	struct sigaction childFinished;
 
-	printf("Welcome to MaSh, enter a command or type h for help.\n");
+	childFinished.sa_sigaction = endSignal;
+
+	input = malloc(sizeof(char)*50);
+	printf("Welcome to MaSh, enter a command.\n");
 	printf(">");
 	fgets(input,50,stdin);
+	sigaction(SIGCHLD, &childFinished, NULL);
 	while(strcmp(input,"exit\n") != 0) {
 		runInBG = 0;
 		redirToFile = 0;
 		commandTwo = 0;
 		char **args = calloc(10,sizeof(char*));
+		//parse user input into argument array
+		status = parseInput(input,args);
+		if (status > 1)
+			runInBG = runInBackground(args,status);
+		if (status > 2)
+			redirToFile = redirectToFromFile(runInBG,args,status, &commandTwo);
 		childpid = fork();
 		if (childpid >= 0) { //fork success
-			status = parseInput(input,args);
-			if (status > 1)
-				runInBG = runInBackground(args,status);
-			if (status > 2)
-				redirToFile = redirectToFromFile(runInBG,args,status, &commandTwo);
 			if (childpid == 0) {
 				if (redirToFile == 1) {
 					execStatus = writeOptToFile(runInBG,args,status);
@@ -103,7 +144,6 @@ int main(int argc, char const *argv[])
 				else if (redirToFile == 2) {
 					execStatus = readIptFromFile(runInBG,args,status);
 				}
-				//write code to pipe here
 				else if (redirToFile == 3) {
 					//create pipe
 					if (pipe(pipefd) == -1) {
@@ -111,6 +151,7 @@ int main(int argc, char const *argv[])
 					}
 					pipeChild = fork();
 					if (pipeChild >= 0) {
+						//read from the pipe
 						if (pipeChild == 0) {
 							close(1);
 							dup(pipefd[1]);
@@ -118,6 +159,7 @@ int main(int argc, char const *argv[])
 							execStatus = execvp(*args,args);
 							exit(0);
 						}
+						//write to the pipe
 						else {
 							close(0);
 							dup(pipefd[0]);
@@ -126,10 +168,12 @@ int main(int argc, char const *argv[])
 						}
 					}
 					else {
-						printf("Fork failed.\n");
+						perror("Fork failed.");
+						exit(-1);
 					}
 				}
 				else {
+					//this is supposed to make the output of commands not appear on screen, it doesnt work though
 					if (runInBG == 1) {
 						setpgid(0,0);
 					}
@@ -144,6 +188,7 @@ int main(int argc, char const *argv[])
 				//may not need to do this
 				if (runInBG == 1) {
 					waitpid(-1,&status,WNOHANG | WUNTRACED);
+					runInBG = 0;
 				}
 				else
 					waitpid(childpid,&status,0);			
@@ -156,6 +201,7 @@ int main(int argc, char const *argv[])
 		fgets(input,50,stdin);
 		free(args);
 	}
+	kill(childpid,SIGKILL);
 	printf("logout\n");
 	printf("\n");
 	printf("[Process completed]\n");
